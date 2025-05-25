bb7368923766007280bc77ab4d951e859a66ccc2
rohitmahajan200 <rm90087@gmail.com>
2025-05-25 14:34:25 +0530

    connect to mongoDB

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+export const connetDB=async()=>{
+    try {
+        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        
+        console.log(`\n MongoDB connected! DB Host:-${connectionInstance.connection.name}`);
+        
+    } catch (error) {
+        console.log("Error in DB connection=>",error);
+    }
+}

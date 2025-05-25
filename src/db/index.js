553822877418d728eb338c0165ef1c9b95bb67af
rohitmahajan200 <rm90087@gmail.com>
2025-05-25 22:43:36 +0530

    add utility

--- src/db/index.js
+++ src/db/index.js
@@ -4,8 +4,7 @@ import { DB_NAME } from "../constants.js";
 export const connetDB=async()=>{
     try {
         const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        
-        console.log(`\n MongoDB connected! DB Host:-${connectionInstance.connection.name}`);
+        console.log(`\nMongoDB connected! DB Host:-${connectionInstance.connection.name}`);
         
     } catch (error) {
         console.log("Error in DB connection=>",error);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import dotenv from 'dotenv';
 import { connetDB } from './db/index.js'
+import app from './app.js';
 
 dotenv.config({path:'./env'})
-connetDB();
+
+connetDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongo Db connection failed!!",err);
+    
+})
